1df8eff43ecff7adae5252e1733081f1b243f3b8
Jan Kopański <j.kopanski@students.mimuw.edu.pl>
2016-05-24 21:36:12 +0200

    udp tcp

--- player.cpp
+++ player.cpp
@@ -5,29 +5,33 @@
 #include <poll.h>
 #include <unistd.h>
 #include <string>
+#include <boost/regex>
 #include "err.h"
 
-using namespace std;
+const int QUEUE_LENGTH = 5;
 
 int main(int argc, char *argv[]) {
     if (argc != 7) {
         fatal("Usage: %s host path r-port file m-port md", argv[0]);
     }
 
-    char *host = argv[1];
-    char *path = argv[2];
-    char *r_port = argv[3];
-    char *file = argv[4];
-    char *m_port = argv[5];
-    char *md = argv[6];
+    const char *host = argv[1];
+    const char *path = argv[2];
+    const char *r_port = argv[3];
+    const char *file = argv[4];
+    const char *m_port = argv[5];
+    const char *md = argv[6];
 
     int r_sock, m_sock;
+    int m_port_int;
     int rc;
+    int len;
     struct addrinfo addr_hints, *addr_result;
+    struct sockaddr_in controller;
 
     r_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (r_sock < 0) {
-        syserr("socket");
+        syserr("radio socket");
     }
 
     memset(&addr_hints, 0, sizeof(struct addrinfo));
@@ -44,12 +48,44 @@ int main(int argc, char *argv[]) {
 
     rc = connect(r_sock, addr_result->ai_addr, addr_result->ai_addrlen);
     if (rc != 0) {
-        syserr("connect");
+        syserr("radio connect");
+    }
+    freeaddrinfo(addr_result);
+
+
+    m_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (m_sock < 0) {
+        syserr("message socket");
     }
 
-    // UDP
+    if (boost::regex_match(m_port, boost::regex("\\d+"))) {
+        m_port_int = atoi(m_port);
+        if (m_port_int < 1024 || m_port_int > 65535) {
+            fatal("Invalid port number: %d", m_port_int);
+        }
+    }
+    else {
+        fatal("Invalid port number: %s", m_port);
+    }
 
-    freeaddrinfo(addr_result);
+    controller.sin_family = AF_INET;
+    controller.sin_addr.s_addr = htonl(INADDR_ANY);
+    controller.sin_port = htons(m_port_int);
+    rc = bind(m_sock, (struct sockaddr *) &controller, (socklen_t) sizeof(controller));
+    if (rc < 0) {
+        syserr("Binding stream socket");
+    }
+
+    len = sizeof(controller);
+    rc = getsockname(m_sock, (struct sockaddr *) &controller, (socklen_t *) &len);
+    if (rc < 0) {
+        syserr("Getting socket name");
+    }
+
+    rc = listen(m_sock, QUEUE_LENGTH);
+    if (rc < 0) {
+        syserr("Starting to listen");
+    }
 
     string get = "";
     get += "GET / HTTP/1.0\r\n";
