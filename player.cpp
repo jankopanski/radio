1a017c6304aa5caa8fec833e4fd1421cf700c9d6
Jan Kopański <j.kopanski@students.mimuw.edu.pl>
2016-05-24 22:19:29 +0200

    functions

--- player.cpp
+++ player.cpp
@@ -10,24 +10,9 @@
 
 const int QUEUE_LENGTH = 5;
 
-int main(int argc, char *argv[]) {
-    if (argc != 7) {
-        fatal("Usage: %s host path r-port file m-port md", argv[0]);
-    }
-
-    const char *host = argv[1];
-    const char *path = argv[2];
-    const char *r_port = argv[3];
-    const char *file = argv[4];
-    const char *m_port = argv[5];
-    const char *md = argv[6];
-
-    int r_sock, m_sock;
-    int m_port_int;
-    int rc;
-    int len;
+int initialize_radio_socket(const char *host, const char *r_port) {
+    int r_sock, rc;
     struct addrinfo addr_hints, *addr_result;
-    struct sockaddr_in controller;
 
     r_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (r_sock < 0) {
@@ -52,6 +37,12 @@ int main(int argc, char *argv[]) {
     }
     freeaddrinfo(addr_result);
 
+    return r_sock;
+}
+
+int initialize_message_socket(const char *m_port) {
+    int m_sock, m_port_int, rc;
+    struct sockaddr_in controller;
 
     m_sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (m_sock < 0) {
@@ -70,42 +61,62 @@ int main(int argc, char *argv[]) {
 
     controller.sin_family = AF_INET;
     controller.sin_addr.s_addr = htonl(INADDR_ANY);
-    controller.sin_port = htons(m_port_int);
+    controller.sin_port = htons((uint16_t) m_port_int);
     rc = bind(m_sock, (struct sockaddr *) &controller, (socklen_t) sizeof(controller));
     if (rc < 0) {
         syserr("Binding stream socket");
     }
 
-    len = sizeof(controller);
-    rc = getsockname(m_sock, (struct sockaddr *) &controller, (socklen_t *) &len);
-    if (rc < 0) {
-        syserr("Getting socket name");
-    }
+//    socklen_t len = sizeof(controller);
+//    rc = getsockname(m_sock, (struct sockaddr *) &controller, &len);
+//    if (rc < 0) {
+//        syserr("Getting socket name");
+//    }
 
     rc = listen(m_sock, QUEUE_LENGTH);
     if (rc < 0) {
         syserr("Starting to listen");
     }
+}
 
-    string get = "";
-    get += "GET / HTTP/1.0\r\n";
-    //get += "Host: 66.220.31.135\r\n";
-    get += "Host: stream3.polskieradio.pl\r\n";
-    //get += "User-Agent: Orban/Coding Technologies AAC/aacPlus Plugin 1.0 (os=Windows NT 5.1 Service Pack 2)\r\n";
-    get += "User-Agent: MPlayer 2.0-728-g2c378c7-4build1\r\n";
-    //get += "Accept: */*\r\n";
-    //get += "Icy-MetaData:1\r\n";
-    get += "Icy-MetaData:0\r\n";
-    //get += "Connection: close\r\n";
-    get += "\r\n";
-
-    write(r_sock, get.c_str(), get.size());
-
-    char buffer[100000];
-
-    int len = 0;
-    for (/*int num = 0; num < 1000; num += len*/;;) {
-        len = read(r_sock, buffer, 100000);
-        write(1, buffer, len);
+int main(int argc, char *argv[]) {
+    if (argc != 7) {
+        fatal("Usage: %s host path r-port file m-port md", argv[0]);
     }
+
+    const char *host = argv[1];
+    const char *path = argv[2];
+    const char *r_port = argv[3];
+    const char *file = argv[4];
+    const char *m_port = argv[5];
+    const char *md = argv[6];
+
+    int r_sock, m_sock;
+
+    r_sock = initialize_radio_socket(host, r_port);
+    m_sock = initialize_message_socket(m_port);
+
+
+
+//    string get = "";
+//    get += "GET / HTTP/1.0\r\n";
+//    //get += "Host: 66.220.31.135\r\n";
+//    get += "Host: stream3.polskieradio.pl\r\n";
+//    //get += "User-Agent: Orban/Coding Technologies AAC/aacPlus Plugin 1.0 (os=Windows NT 5.1 Service Pack 2)\r\n";
+//    get += "User-Agent: MPlayer 2.0-728-g2c378c7-4build1\r\n";
+//    //get += "Accept: */*\r\n";
+//    //get += "Icy-MetaData:1\r\n";
+//    get += "Icy-MetaData:0\r\n";
+//    //get += "Connection: close\r\n";
+//    get += "\r\n";
+//
+//    write(r_sock, get.c_str(), get.size());
+//
+//    char buffer[100000];
+//
+//    int len = 0;
+//    for (/*int num = 0; num < 1000; num += len*/;;) {
+//        len = read(r_sock, buffer, 100000);
+//        write(1, buffer, len);
+//    }
 }
